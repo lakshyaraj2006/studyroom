e147a60705b521752b716b93a0a3cbafeaaa7430
Lakshyaraj Dash <189126809+lakshyaraj2006@users.noreply.github.com>
2025-12-14 10:25:20 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StudyRoom - Group Study Made Easy
+
+More description to be added later.
---
+++ packages/server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Initialize server!"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`StudyRoom server listening on port ${port}`);
+})
