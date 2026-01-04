b661b8a55ce0a0646455969d89a512edc73aa20b
Lakshyaraj Dash <189126809+lakshyaraj2006@users.noreply.github.com>
2026-01-04 17:34:07 +0530

    Add middleware protection

--- packages/server/src/routes/user.route.ts
+++ packages/server/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller";
+import { checkAuth } from "../middlewares";
 
 const router = Router();
 
 router.route("/create").post(userController.createUser);
 router.route("/login").post(userController.loginUser);
-router.route("/logout").post(userController.logoutUser);
+router.route("/logout").post(checkAuth, userController.logoutUser);
 router.route("/rotate").post(userController.rotateAccessAndRefreshTokens);
 
 export { router as userRouter };
